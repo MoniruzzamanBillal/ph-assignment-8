d59ca96a060dd2052e3baa3cf6f938b84edbf162
Md Moniruzzaman <mdmoniruzzamanbillal@gmail.com>
2023-09-26 00:11:43 +0600

    fixing chart data

--- src/Pages/Statistics.jsx
+++ src/Pages/Statistics.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { Chart } from "react-google-charts";
+
 import { useLoaderData } from "react-router-dom";
 import { getDonatedId } from "../utility/LocalStorage";
+import { PieChart } from "@mui/x-charts/PieChart";
 
 const Statistics = () => {
   const responseData = useLoaderData();
@@ -11,24 +12,50 @@ const Statistics = () => {
   const totalLength = responseData.length;
   const totalDonated = donatedData.length;
 
-  const data = [
-    ["Task", "Hours per Day"],
-    ["Total Donation", totalLength],
-    ["My Donation", totalDonated],
-  ];
+  const donationPercentage = ((totalDonated * 100) / totalLength).toFixed(2);
+  const remaining = 100 - donationPercentage;
 
   return (
     <div className="statisticsContainer  ">
       <div className="statisticsWrapper  w-[95%] sm:w-[90%] m-auto ">
-        <Chart
-          chartType="PieChart"
-          data={data}
-          width={"100%"}
-          height={"400px"}
-        />
+        <div className="chartContainer  flex mt-6 ">
+          <PieChart
+            series={[
+              {
+                data: [
+                  { id: 0, value: donationPercentage, label: "My Donation" },
+                  {
+                    id: 1,
+                    value: remaining,
+                    label: "Total Donation",
+                    color: "red",
+                  },
+                ],
+              },
+            ]}
+            width={400}
+            height={200}
+          />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Statistics;
+
+{
+  /* <PieChart
+  series={[
+    {
+      data: [
+        { id: 0, value: 10, label: 'series A' },
+        { id: 1, value: 15, label: 'series B' },
+        { id: 2, value: 20, label: 'series C' },
+      ],
+    },
+  ]}
+  width={400}
+  height={200}
+/> */
+}
